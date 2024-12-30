732047e19f5705c418b6bdbf644d5c567bcf45a6
0xminty <107131917+0xminty@users.noreply.github.com>
2024-12-30 17:44:49 -0500

    Update hw.sol

--- hw.sol
+++ hw.sol
@@ -2,6 +2,28 @@
 // compiler version must be greater than or equal to 0.8.26 and less than 0.9.0
 pragma solidity ^0.8.26;
 
-contract HelloWorld {
-    string public greet = "Hello World!";
+contract Trans {
+    bool transient x;
+    function store() public returns(bool res){
+    assembly{
+    tstore(x.slot,true,)
+    res:=tload(x.slot)
+ }
+}
+
+}
+
+
+
+Contract Caller{
+Trans t;
+
+function getTrans() external {
+//encode data for call
+bytes memory data = abi.encodeWithSignature("store()");
+//want to call the contract that sets transient storage 
+
+t.call(data);
+}
+
 }
